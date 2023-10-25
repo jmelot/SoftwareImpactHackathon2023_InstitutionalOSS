d56019418af19e47dadfe33d76abf2f8f5f1949b
kaitlynhair <kaitlyn.hair@ed.ac.uk>
2023-10-25 19:52:03 +0100

    example R script for using doi to fetch metadata from openalex (including ror)

---
+++ openalex_doi_to_ror.R
@@ -0,0 +1,15 @@
+# simple demo script for image J example 
+library(openalexR)
+
+doi_example <- "https://doi.org/10.1038/nmeth.2089"
+
+# openalex connection
+paper <- works_from_dois <- oa_fetch(
+  entity = "works",
+  doi = doi_example,
+  verbose = TRUE
+)
+
+# see ROR nested in author list
+paper$author
+
